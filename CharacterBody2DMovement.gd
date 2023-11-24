79f01107bb500f5ebcc71c74367a100eec6fae66
zockerhuhn <fbxfbxfbx5@gmail.com>
2023-11-24 17:28:30 +0100

    slows down correctly and faster if steered against

--- CharacterBody2DMovement.gd
+++ CharacterBody2DMovement.gd
@@ -6,24 +6,24 @@ var yMovement = 0
 var xMovement = 0
 
 func get_input():
-	if Input.is_action_pressed("left") and xMovement <= 498:
+	if Input.is_action_pressed("left") and xMovement >= -498:
 		xMovement -= 4
-	if Input.is_action_pressed("right") and xMovement >= -498:
+	if Input.is_action_pressed("right") and xMovement <= 498:
 		xMovement += 4
-	if Input.is_action_pressed("up") and yMovement <= 498:
+	if Input.is_action_pressed("up") and yMovement >= -498:
 		yMovement -= 4
-	if Input.is_action_pressed("down") and yMovement >= -498:
+	if Input.is_action_pressed("down") and yMovement <= 498:
 		yMovement += 4
-	if not (xMovement <= 0 and Input.is_action_pressed("right")):
+	if xMovement >= 1 and not Input.is_action_pressed("right"):
 		xMovement -= 2
 		print("test1")
-	if not (xMovement >= 0 and Input.is_action_pressed("left")):
+	if xMovement <= 0 and not Input.is_action_pressed("left"):
 		xMovement += 2
 		print("test2")
-	if not (yMovement <= 0 and Input.is_action_pressed("up")):
+	if yMovement >= 0 and not Input.is_action_pressed("up"):
 		yMovement -= 2
 		print("test3")
-	if not (yMovement >= 0 and Input.is_action_pressed("down")):
+	if yMovement <= 0 and not Input.is_action_pressed("down"):
 		yMovement += 2
 		print("test4")
 	velocity = Vector2(xMovement, yMovement)
