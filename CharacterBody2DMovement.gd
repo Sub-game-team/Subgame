c6dfe3463b2651ba7353c71cec5b4f14d3d6feb3
zockerhuhn <fbxfbxfbx5@gmail.com>
2023-11-24 17:39:52 +0100

    removed test prints

--- CharacterBody2DMovement.gd
+++ CharacterBody2DMovement.gd
@@ -16,23 +16,13 @@ func get_input():
 		yMovement += 4
 	if xMovement >= 1 and not Input.is_action_pressed("right"):
 		xMovement -= 2
-		print("test1")
 	if xMovement <= 0 and not Input.is_action_pressed("left"):
 		xMovement += 2
-		print("test2")
 	if yMovement >= 0 and not Input.is_action_pressed("up"):
 		yMovement -= 2
-		print("test3")
 	if yMovement <= 0 and not Input.is_action_pressed("down"):
 		yMovement += 2
-		print("test4")
 	velocity = Vector2(xMovement, yMovement)
-	print(Input.is_action_pressed("left"))
-	print(Input.is_action_pressed("right"))
-	print(Input.is_action_pressed("up"))
-	print(Input.is_action_pressed("down"))
-	print(xMovement)
-	print(yMovement)
 	
 func _physics_process(_delta):
 	get_input()
