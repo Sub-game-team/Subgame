2bfbd13bab6d31facabe8a1901234022675ca7f7
zockerhuhn <fbxfbxfbx5@gmail.com>
2023-11-27 02:34:57 +0100

    removed annoying debug warnings

--- player_camera.gd
+++ player_camera.gd
@@ -9,7 +9,7 @@ func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	
 	# Check for mouse wheel input
 	var zoom_input = 0.0
