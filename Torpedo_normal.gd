ac7e41a2bd292ecac2d24d672f677bbef89caa51
zockerhuhn <fbxfbxfbx5@gmail.com>
2023-11-28 18:16:43 +0100

    removed print

--- Torpedo_normal.gd
+++ Torpedo_normal.gd
@@ -9,8 +9,6 @@ func _on_timer_timeout():
 	queue_free()
 
 func _on_area_2d_area_entered(area):
-	print("test1")
 	if area.is_in_group("enemy"):
-		print("test2")
 		area.get_parent().queue_free()
 		queue_free()
